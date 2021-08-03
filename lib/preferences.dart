78ec0e7e008de9b1e97158cefbb0190dfc9bd31b
Farham Ahmad <farham329@gmail.com>
2021-08-03 17:34:14 +0100

    added theme preferences

--- lib/preferences.dart
+++ lib/preferences.dart
@@ -1,8 +1,24 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 class Preferences {
   /// intially the app is on light theme
   /// changes the theme everytime the function is called
-  static var isDarkTheme = false;
-  static Future<void> toggleTheme() async {
-    isDarkTheme = !isDarkTheme;
+  static Future<bool> getTheme() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    // returns false if value does not exist
+    return prefs.getBool('isDarkTheme') ?? false;
+  }
+
+  /// set the theme of the app
+  toggleTheme() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool? isDark = prefs.getBool('isDarkTheme');
+    bool newValue;
+    if (isDark == null) {
+      newValue = false;
+    } else {
+      newValue = !isDark;
+    }
+    prefs.setBool('isDarkTheme', newValue);
   }
 }
