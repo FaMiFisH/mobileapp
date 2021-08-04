bb685743fdba488e4a4d876c74e955125cf1dc68
Farham Ahmad <farham329@gmail.com>
2021-08-04 14:05:57 +0100

    implemented a change notifier provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,32 @@
+import 'package:alevelcoursework/themes.dart';
 import 'package:flutter/material.dart';
-import 'interfaces/home.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'interfaces/HomeScreen.dart';
 import 'interfaces/RegisterScreen.dart';
 import 'interfaces/LoginScreen.dart';
-//import 'assets/constants.dart' as constants;
-import 'package:alevelcoursework/assets/constants.dart' as constants;
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  runApp(new MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "A-level Coursework",
-        // light theme
-        theme: ThemeData(
-          brightness: Brightness.light,
-          // colors
-          primaryColorLight: constants.l_primaryColor,
-          primaryColorDark: constants.l_secondaryColor,
-          canvasColor: constants.l_canvasColor,
-          accentColor: constants.l_accentColor,
-          hintColor: Colors.grey,
-          shadowColor: constants.shadowColor,
-          errorColor: Colors.red,
-
-          // font
-          fontFamily: "Montserrat",
-          textTheme: TextTheme(
-              headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-              headline2: TextStyle(fontSize: 36.0),
-              bodyText1: TextStyle(fontSize: 14.0)),
-        ),
-        // TODO: dark theme and maybe global padding
-        //darkTheme: ThemeData(),
-        routes: <String, WidgetBuilder>{
-          "Register": (BuildContext context) => new RegisterScreen(),
-          "Home": (BuildContext context) => new HomePage()
-        },
-        home: LoginScreen());
-  }
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      builder: (context, _) {
+        final themeProvider = Provider.of<ThemeProvider>(context);
+        return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: "A-level Coursework",
+            themeMode: themeProvider.themeMode,
+            theme: MyThemes.lightThemeData,
+            darkTheme: MyThemes.darkThemeData,
+            routes: <String, WidgetBuilder>{
+              "Login": (BuildContext context) => new LoginScreen(),
+              "Register": (BuildContext context) => new RegisterScreen(),
+              "Home": (BuildContext context) => new HomePage()
+            },
+            home: LoginScreen());
+      });
 }
