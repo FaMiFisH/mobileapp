cb347531173e830b636decafc4ab2c34ae85e0ec
Farham Ahmad <farham329@gmail.com>
2021-08-05 13:10:47 +0100

    added theme based styling

--- lib/interfaces/LoginScreen.dart
+++ lib/interfaces/LoginScreen.dart
@@ -1,6 +1,8 @@
 import 'package:alevelcoursework/interfaces/components/themeSwitch.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../themes/themes.dart';
 import 'HomeScreen.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -49,159 +51,154 @@ class _LoginScreenState extends State<LoginScreen> {
   // -Build method---------
   @override
   Widget build(BuildContext context) {
+    // gets the theme mode
+    ThemeMode themeMode = Provider.of<ThemeProvider>(context).themeMode;
+
+    //gets the device data
+    double deviceWidth = MediaQuery.of(context).size.width;
+    double deviceHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-        body: SafeArea(
-            child: Padding(
-                padding: const EdgeInsets.all(32.0),
-                child: Center(
-                    child: ConstrainedBox(
-                        constraints:
-                            BoxConstraints(maxHeight: 500.0, maxWidth: 350.0),
-                        child: Container(
-                          padding: EdgeInsets.all(20.0),
-                          decoration: BoxDecoration(
-                              color: Theme.of(context).primaryColorLight,
-                              border: Border(),
-                              boxShadow: [
-                                BoxShadow(
-                                    color: Theme.of(context).shadowColor,
-                                    spreadRadius: 2),
-                              ],
-                              borderRadius: BorderRadius.circular(24)),
-                          child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                //Logo
-                                Container(
-                                    // TODO: add the image logo instead of text
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Center(
+                child: ConstrainedBox(
+                    constraints:
+                        BoxConstraints(maxHeight: 500.0, maxWidth: 350.0),
+                    child: Container(
+                      padding: EdgeInsets.all(20.0),
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColorLight,
+                          boxShadow: [
+                            BoxShadow(
+                                color: Theme.of(context).shadowColor,
+                                spreadRadius: 1),
+                          ],
+                          borderRadius: BorderRadius.circular(24)),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Container(
+                                child: Text(
+                              "Login",
+                              style: Theme.of(context).textTheme.headline1,
+                            )),
+                            Container(
+                                child: Column(children: <Widget>[
+                              TextFormField(
+                                controller: emailController,
+                                decoration: InputDecoration(
+                                    labelText: "Email",
+                                    labelStyle: TextStyle(
+                                        color: Theme.of(context).hintColor),
+                                    focusedBorder: UnderlineInputBorder(
+                                        borderSide: BorderSide(
+                                            color:
+                                                Theme.of(context).accentColor)),
+                                    prefixIcon: Icon(Icons.email,
+                                        color:
+                                            Theme.of(context).primaryColorDark),
+                                    suffixIcon: emailController.text.isNotEmpty
+                                        ? IconButton(
+                                            icon: Icon(Icons.clear,
+                                                color: Theme.of(context)
+                                                    .primaryColorDark),
+                                            onPressed: () =>
+                                                {emailController.clear()},
+                                          )
+                                        : SizedBox.shrink()),
+                              ),
+                              SizedBox(height: 8.0),
+                              TextFormField(
+                                controller: passController,
+                                decoration: InputDecoration(
+                                    labelText: "Password",
+                                    labelStyle: TextStyle(
+                                        color: Theme.of(context).hintColor),
+                                    focusedBorder: UnderlineInputBorder(
+                                        borderSide: BorderSide(
+                                            color:
+                                                Theme.of(context).accentColor)),
+                                    prefixIcon: Icon(Icons.lock,
+                                        color:
+                                            Theme.of(context).primaryColorDark),
+                                    suffixIcon: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: passController.text.isNotEmpty
+                                          ? [
+                                              IconButton(
+                                                  icon: Icon(Icons.clear,
+                                                      color: Theme.of(context)
+                                                          .primaryColorDark),
+                                                  onPressed: () =>
+                                                      {passController.clear()}),
+                                              IconButton(
+                                                  icon: Icon(
+                                                    hidePass
+                                                        ? Icons.visibility_off
+                                                        : Icons.visibility,
+                                                    color: Theme.of(context)
+                                                        .primaryColorDark,
+                                                  ),
+                                                  onPressed: () =>
+                                                      toggleVisibility()),
+                                            ]
+                                          : [],
+                                    )),
+                                obscureText: hidePass,
+                                enableSuggestions: false,
+                                autocorrect: false,
+                              ),
+                              SizedBox(height: 8.0),
+                              Container(
+                                  alignment: Alignment(1.0, 0.0),
+                                  padding: EdgeInsets.only(top: 12.0),
+                                  child: InkWell(
+                                      child: Text("Forgot Password?",
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .subtitle2)))
+                            ])),
+                            SizedBox(height: 32.0),
+                            //buttons
+                            Container(
+                              child: Column(children: [
+                                ElevatedButton(
+                                    onPressed: () => runApp(HomePage()),
                                     child: Text("Login",
                                         style: Theme.of(context)
                                             .textTheme
-                                            .headline1
-                                        //TextStyle(fontSize: 48.0, fontWeight: FontWeight.bold),
-                                        )),
-                                //Text boxes
-                                Container(
-                                    //padding: EdgeInsets.fromLTRB(20.0, 32.0, 20.0, 0.0),
-                                    child: Column(children: <Widget>[
-                                  TextFormField(
-                                    controller: emailController,
-                                    decoration: InputDecoration(
-                                        labelText: "Email",
-                                        labelStyle: TextStyle(
-                                            color: Theme.of(context).hintColor),
-                                        focusedBorder: UnderlineInputBorder(
-                                            borderSide: BorderSide(
-                                                color: Theme.of(context)
-                                                    .accentColor)),
-                                        prefixIcon: Icon(Icons.email,
-                                            color: Theme.of(context)
-                                                .primaryColorDark),
-                                        suffixIcon: emailController
-                                                .text.isNotEmpty
-                                            ? IconButton(
-                                                icon: Icon(Icons.clear,
-                                                    color: Theme.of(context)
-                                                        .primaryColorDark),
-                                                onPressed: () =>
-                                                    {emailController.clear()},
-                                              )
-                                            : SizedBox.shrink()),
-                                  ),
-                                  SizedBox(height: 8.0),
-                                  TextFormField(
-                                    controller: passController,
-                                    decoration: InputDecoration(
-                                        labelText: "Password",
-                                        labelStyle: TextStyle(
-                                            color: Theme.of(context).hintColor),
-                                        focusedBorder: UnderlineInputBorder(
-                                            borderSide: BorderSide(
-                                                color: Theme.of(context)
-                                                    .accentColor)),
-                                        prefixIcon: Icon(Icons.lock,
-                                            color: Theme.of(context)
-                                                .primaryColorDark),
-                                        suffixIcon: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceBetween,
-                                          mainAxisSize: MainAxisSize.min,
-                                          children: passController
-                                                  .text.isNotEmpty
-                                              ? [
-                                                  IconButton(
-                                                      icon: Icon(Icons.clear,
-                                                          color: Theme.of(
-                                                                  context)
-                                                              .primaryColorDark),
-                                                      onPressed: () => {
-                                                            passController
-                                                                .clear()
-                                                          }),
-                                                  IconButton(
-                                                      icon: Icon(
-                                                        hidePass
-                                                            ? Icons
-                                                                .visibility_off
-                                                            : Icons.visibility,
-                                                        color: Theme.of(context)
-                                                            .primaryColorDark,
-                                                      ),
-                                                      onPressed: () =>
-                                                          toggleVisibility()),
-                                                ]
-                                              : [],
-                                        )),
-                                    obscureText: hidePass,
-                                    enableSuggestions: false,
-                                    autocorrect: false,
-                                  ),
-                                  SizedBox(height: 8.0),
-                                  Container(
-                                      alignment: Alignment(1.0, 0.0),
-                                      padding: EdgeInsets.only(top: 12.0),
-                                      child: InkWell(
-                                          child: Text("Forgot Password?",
-                                              style: TextStyle(
-                                                  color: Theme.of(context)
-                                                      .primaryColorDark,
-                                                  fontWeight: FontWeight.bold,
-                                                  decoration: TextDecoration
-                                                      .underline))))
-                                ])),
-                                SizedBox(height: 48.0),
-                                //buttons
-                                Container(
-                                  child: Column(children: [
-                                    ElevatedButton(
-                                      onPressed: () => runApp(HomePage()),
-                                      child: Text("Login"),
-                                    ),
-                                    SizedBox(height: 12.0),
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: <Widget>[
-                                        Text("Don't have an account?"),
-                                        SizedBox(width: 5.0),
-                                        InkWell(
-                                            onTap: () {
-                                              Navigator.of(context)
-                                                  .pushNamed("Register");
-                                            },
-                                            child: Text("Register",
-                                                style: TextStyle(
-                                                    color: Theme.of(context)
-                                                        .primaryColorDark,
-                                                    fontWeight: FontWeight.bold,
-                                                    decoration: TextDecoration
-                                                        .underline)))
-                                      ],
-                                    )
-                                  ]),
-                                ),
+                                            .button)),
+                                SizedBox(height: 12.0),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Text("Don't have an account?",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyText2),
+                                    SizedBox(width: 5.0),
+                                    InkWell(
+                                        onTap: () {
+                                          Navigator.of(context)
+                                              .pushNamed("Register");
+                                        },
+                                        child: Text("Register",
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .subtitle2))
+                                  ],
+                                )
                               ]),
-                        ))))));
+                            ),
+                          ]),
+                    )))),
+      )),
+    );
   }
 }
