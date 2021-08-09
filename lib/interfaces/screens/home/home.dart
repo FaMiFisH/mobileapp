f42de6ff4d553b3ab2ce508d0b4479d6e45fa662
Farham Ahmad <farham329@gmail.com>
2021-08-10 00:03:52 +0100

    wrapped in a change notifier provider

--- lib/interfaces/screens/home/home.dart
+++ lib/interfaces/screens/home/home.dart
@@ -1,9 +1,12 @@
 import 'package:alevelcoursework/interfaces/components/themeSwitch.dart';
+import 'package:alevelcoursework/themes/themes.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../../responsive.dart';
 import '../../../interfaces/components/sideMenu.dart';
+import '../login.dart';
 import 'homeBody.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,27 +16,38 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
-  Widget build(BuildContext context) {
-    /// allows to know the height and width of the device
-    Size deviceSize = MediaQuery.of(context).size;
+  //Size deviceSize = MediaQuery.of(context).size;
+  Widget build(BuildContext context) => ChangeNotifierProvider(
 
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text("Home page"),
-              //backgroundColor: Theme.of(context).canvasColor,
-              actions: <Widget>[ThemeSwitch()],
-            ),
-            body: Row(children: <Widget>[
-              Expanded(
-                flex: deviceSize.width > 1000 ? 2 : 4,
-                child: SideMenu(),
-              ),
-              Expanded(
-                flex: deviceSize.width > 1000 ? 8 : 10,
-                child: HomeBody(),
-              )
-            ])));
-  }
+      /// allows to know the height and width of the device
+      create: (context) => ThemeProvider(),
+      builder: (context, _) {
+        final themeProvider = Provider.of<ThemeProvider>(context);
+        return new MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: "A-level Coursework",
+            themeMode: themeProvider.themeMode,
+            theme: MyThemes.lightThemeData,
+            darkTheme: MyThemes.darkThemeData,
+            routes: <String, WidgetBuilder>{
+              "Login": (BuildContext context) => new LoginPage(),
+              "Home": (BuildContext context) => new HomePage()
+            },
+            home: Scaffold(
+                appBar: AppBar(
+                  title: Text("Home page"),
+                  //backgroundColor: Theme.of(context).canvasColor,
+                  actions: <Widget>[ThemeSwitch()],
+                ),
+                body: Row(children: <Widget>[
+                  Expanded(
+                    //flex: deviceSize.width > 1000 ? 2 : 4,
+                    child: SideMenu(),
+                  ),
+                  Expanded(
+                    //flex: deviceSize.width > 1000 ? 8 : 10,
+                    child: HomeBody(),
+                  )
+                ])));
+      });
 }
