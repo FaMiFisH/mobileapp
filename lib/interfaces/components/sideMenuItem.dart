a3d29a712d6ac50bc65bd25dd67238ce797e9b6c
Farham Ahmad <farham329@gmail.com>
2021-08-17 23:46:01 +0100

    implemented side menu item class

--- lib/interfaces/components/sideMenuItem.dart
+++ lib/interfaces/components/sideMenuItem.dart
@@ -5,17 +5,16 @@ import '../../constants.dart';
 
 class SideMenuItem extends StatelessWidget {
   final bool isActive, isHover, showBorder;
-  final int itemCount;
-  final String iconSrc, title;
+  final IconData icon;
+  final String title;
   final VoidCallback press;
 
   const SideMenuItem({
-    required Key key,
+    Key? key,
     this.isActive = false,
     this.isHover = false,
-    required this.itemCount,
     this.showBorder = true,
-    required this.iconSrc,
+    required this.icon,
     required this.title,
     required this.press,
   }) : super(key: key);
@@ -23,14 +22,45 @@ class SideMenuItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(defaultPadding),
+      padding: const EdgeInsets.all(0.0),
       child: InkWell(
         onTap: press,
         child: Row(
           children: <Widget>[
-            (isActive || isHover)
-                ? Image(image: AssetImage("assets/logo.png"))
-                : SizedBox(width: 15),
+            // (isActive || isHover)
+            //     ? Image(image: AssetImage("assets/logo.png"))
+            //     : SizedBox(width: 15),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.only(bottom: 15, right: 5),
+                decoration: showBorder
+                    ? BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(color: Color(0xFFDFE2EF)),
+                        ),
+                      )
+                    : null,
+                child: Row(
+                  children: [
+                    Icon(
+                      icon,
+                      color: (isActive || isHover)
+                          ? Theme.of(context).primaryColorDark
+                          : Colors.grey,
+                      size: 20.0,
+                    ),
+                    SizedBox(width: 8.0),
+                    Text(
+                      title,
+                      style: TextStyle(
+                        color:
+                            (isActive || isHover) ? Colors.white : Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
